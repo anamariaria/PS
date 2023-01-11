ab62c3553701b341bc66750c0c577eef9ef0f2c4
anamaria <anamariachirita16@gmail.com>
2023-01-11 19:02:51 +0200

    getch() pentru a afisa solutiile pe rand

--- PS/problema_regine.c
+++ PS/problema_regine.c
@@ -42,4 +42,27 @@ int main() {
 				i = i - 2; // Decrementeaza valoarea lui i cu 2
 			}
 		}
+
+		//print the solution
+
+		solution_count++;
+
+		printf("Solution %d:\n", solution_count);
+		for (i = 1; i <= n; i++)
+		{
+			for (conflict = 1; conflict <= n; conflict++)
+			{
+				if (conflict == R[i])
+					printf("Q ");
+				else
+					printf("- ");
+			}
+			printf("\n");
+		}
+		printf("\n[");
+		for (i = 1; i < n; i++)
+			printf("%d, ", R[i]);
+		printf("%d]", R[n]);
+		getch();
+
 }
